037c6097b8778ef8668c933ee866ffb105ab3da6
SamGVanN <samuel.vannier@hotmail.fr>
2022-06-05 18:24:02 +0200

    fix : exe version

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from cx_Freeze import setup, Executable
+
 base = None
 executables = [Executable("src/GetReadyToWork.py", base=base)]
 
@@ -15,7 +16,8 @@ options = {
 setup(
     name = "Get Ready To Work",
     options = options,
-    version = "1.0",
+    version = "0.0.2",
     description = 'GetReadyToWork permet de lancer les applications souhaitée en un seul clic ',
     executables = executables
+    
 )
